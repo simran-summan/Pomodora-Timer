3b87700fd6363c1419ec9eb7de6792df5670b53a
Simran Summan <summansimran7@gmail.com>
2024-07-16 14:28:44 +0530

    Add day 1 and day 2

---
+++ OneDrive/Desktop/assigments/js-practice/day1.js
@@ -0,0 +1,32 @@
+// Activity 1
+var a = 12
+// console.log(a);
+
+// Task 2
+let b = 'hello world'
+// console.log(b);
+
+//Task 3
+const bool = false
+// console.log(bool);
+
+// Task 4
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof bool);
+// let arr= ["Sonoo","Vimal","Ratan"]
+// console.log(typeof arr);
+// let obj = {1:'ddd'}
+// console.log(typeof obj);
+
+// Task 5
+let val2 = 3
+console.log(val2);
+val2=14
+console.log(val2);
+
+// task 6
+const val = 8
+console.log(val);
+val = 18
+console.log(val);
---
+++ OneDrive/Desktop/assigments/js-practice/day2.js
@@ -0,0 +1,47 @@
+// Day 2: Operators
+
+// task 1
+let a = 12
+let b = 2
+console.log(a+b);
+// task 2
+console.log(a-b);
+// task 3
+console.log(a*b);
+// task 4
+console.log(a/b);
+// task 5
+console.log(a%b);
+
+// task 6
+b += a
+console.log(b);
+// task 7
+b -= a
+console.log(a);
+
+// task 8
+let x = 4
+let y = 8
+console.log(x>y);
+// task 9
+console.log(x<y);
+// task 10
+let temp1 = 3
+let temp2 = '3'
+console.log(temp1 == temp2);
+console.log(temp1 === temp2);
+
+// task 11
+let val1 = 4
+let val2 = 8
+console.log(val1>val2 && val1<val2);
+// task 12
+console.log(val1>val2 || val1<val2);
+// task 13
+console.log(!(val1>val2));
+
+// Task 14
+let z=-23
+const ternaryOp = z > 0 ? 'positive' : 'negative'
+console.log(ternaryOp);
